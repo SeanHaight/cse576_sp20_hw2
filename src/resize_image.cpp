15e88b9735f8524b56d373db68720c861ab9e58a
Sean Haight <seanhaight1@gmail.com>
2024-04-09 17:47:19 -0700

    Fixed weird issue I was having before. Started problem 1.

--- src/resize_image.cpp
+++ src/resize_image.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include "image.h"
+#include <iostream>
 
 using namespace std;
 
@@ -11,13 +12,14 @@ float Image::pixel_nearest(float x, float y, int c) const
   {
   // Since you are inside class Image you can
   // use the member function pixel(a,b,c)
-  
+  int round_x = (int) x;
+  int round_y = (int) y;
+
+  if (x - round_x >= .5) round_x += 1;
+  if (y - round_y >= .5) round_y += 1;
   // TODO: Your code here
   
-  NOT_IMPLEMENTED();
-  
-  
-  return 0;
+  return clamped_pixel(round_x, round_y, c);
   }
 
 // HW1 #1
@@ -45,7 +47,11 @@ float Image::pixel_bilinear(float x, float y, int c) const
 Image nearest_resize(const Image& im, int w, int h)
   {
   Image ret(w,h,im.c);
-  
+  for (int i = 0; i < w; i ++) for (int j = 0; j < h; j++) for (int ch = 0; ch < im.c; ch ++){
+      ret(i,j,ch) = im.pixel_nearest(i, j, ch);
+  }
+
+  save_image(ret, "output/first_resize_attempt");
   // TODO: Your code here
   
   NOT_IMPLEMENTED();
